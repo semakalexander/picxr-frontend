380f1fa9291ec243630edd9aacf8485c3faa2338
skjerp <semak.alexander@gmail.com>
2019-01-04 11:23:16 +0200

    add delete user

--- src/pages/admin/Users.jsx
+++ src/pages/admin/Users.jsx
@@ -11,6 +11,14 @@ import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import TablePagination from '@material-ui/core/TablePagination';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+import DeleteIcon from '@material-ui/icons/Delete';
 
 import userService from '../../services/users';
 
@@ -39,6 +47,9 @@ const styles = theme => ({
     display: 'flex',
     justifyContent: 'center',
     marginTop: theme.spacing.unit * 6
+  },
+  noRecordsContainer: {
+    marginTop: theme.spacing.unit * 6
   }
 });
 
@@ -47,8 +58,10 @@ class Users extends Component {
     isUsersLoading: true,
     users: [],
     page: 0,
-    perPage: 10,
-    count: 0
+    perPage: 5,
+    count: 0,
+    isDeleteDialogOpen: false,
+    preparedItem: null
   };
     
   componentDidMount() {
@@ -57,6 +70,7 @@ class Users extends Component {
 
   loadUsers = () => {
     const {
+      handleError,
       state: {
         page,
         perPage
@@ -65,25 +79,55 @@ class Users extends Component {
 
     this.setState({ isUsersLoading: true });
 
-    setTimeout(() => {
-      userService
+    userService
       .getAllUsers({ page, perPage })
       .then(({ count, users }) => this.setState({ count, users, isUsersLoading: false }))
-      .catch(error => {
-        console.log({...error});
-        notifications.error(<pre>{JSON.stringify(error.response.data, null, 2)}</pre>)
-      });
-    }, 2000);
+      .catch(error => handleError(error, () => this.setState({ isUsersLoading: false })));
   }
 
   handlePageChange = (e, page) => this.setState({ page }, this.loadUsers);
 
   handlePerPageChange = e => this.setState({ page: 0, perPage: e.target.value }, this.loadUsers);
 
+  prepareForDelete = preparedItem => () =>
+    this.setState({
+      isDeleteDialogOpen: true,
+      preparedItem
+    });
+  
+  closeDeleteDialog = () => this.setState({ isDeleteDialogOpen: false });
+
+  deletePreparedItem = () => {
+    const {
+      handleError,
+      state: {
+        preparedItem
+      }
+    } = this;
+
+    userService
+      .remove(preparedItem._id)
+      .then(() => this.setState({
+        isDeleteDialogOpen: false,
+        preparedItem: null
+      }))
+      .catch(handleError);
+  }
+
+  handleError = (error, cb) => {
+    console.log({...error});
+    notifications.error(<pre>{JSON.stringify(error.response.data, null, 2)}</pre>);
+
+    if (cb && typeof cb === 'function') cb();
+  }
+
   render() {
     const {
       handlePageChange,
       handlePerPageChange,
+      prepareForDelete,
+      closeDeleteDialog,
+      deletePreparedItem,
       props: {
         classes
       },
@@ -92,7 +136,8 @@ class Users extends Component {
         users,
         page,
         perPage,
-        count
+        count,
+        isDeleteDialogOpen
       }
     } = this;
 
@@ -108,15 +153,16 @@ class Users extends Component {
                 </div>
               )
             }
-            
+
             {
-              !!users.length && (
+              !isUsersLoading && (!!users.length ? (
                 <>
                   <Table>
                     <TableHead>
                       <TableRow>
                         <TableCell>Username</TableCell>
                         <TableCell>Email address</TableCell>
+                        <TableCell />
                       </TableRow>
                     </TableHead>
                     <TableBody>
@@ -125,6 +171,9 @@ class Users extends Component {
                           <TableRow key={u._id}>
                             <TableCell>{u.username}</TableCell>
                             <TableCell>{u.email}</TableCell>
+                            <TableCell>
+                              <DeleteIcon className="icon" onClick={prepareForDelete(u)}/>
+                            </TableCell>
                           </TableRow>
                         ))
                       }
@@ -147,9 +196,27 @@ class Users extends Component {
                     onChangeRowsPerPage={handlePerPageChange}
                   />
                 </>
-              )
+              ) : (
+                <div className={classes.noRecordsContainer}>No records yet.</div>
+              ))
             }
           </Paper>
+
+          <Dialog
+            open={isDeleteDialogOpen}
+            onClose={closeDeleteDialog}
+          >
+            <DialogTitle>Are you sure you want to delete this item?</DialogTitle>
+            <DialogContent>
+              <DialogContentText>
+                This action can not be undone. Be sure that you want to do it.
+              </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={closeDeleteDialog} autoFocus>Cancel</Button>
+              <Button onClick={deletePreparedItem} color="primary">Yes, I am sure. Delete it.</Button>
+            </DialogActions>
+          </Dialog>
         </div>
     );
   }
--- src/services/users.js
+++ src/services/users.js
@@ -9,5 +9,15 @@ const getAllUsers = ({ page = 0, perPage = 10, sortOrder = 'asc', sortBy = ''} =
     .get(stringifyQuery('/users', { page, perPage, sortOrder, sortBy }))
     .then(res => res.data);
 
-export { getUserById, getAllUsers };
-export default { getUserById, getAllUsers };
+const remove = id => axios.delete(`/users/${id}`);
+
+export {
+  getUserById,
+  getAllUsers,
+  remove
+};
+export default {
+  getUserById,
+  getAllUsers,
+  remove
+};
